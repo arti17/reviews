7dde983e499a773efe2d829f664fdecc69f3e995
Artem Tkachuk <arti17900@gmail.com>
2019-11-16 12:48:33 +0600

    Добавлены модели товара и отзыва.

--- reviews/webapp/admin.py
+++ reviews/webapp/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from webapp.models import Product, Review
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'description')
+    list_filter = ('category',)
+
+
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('author', 'product', 'description', 'rating')
+    list_filter = ('author',)
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Review, ReviewAdmin)
